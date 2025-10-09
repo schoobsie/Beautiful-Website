62c102769dc56b645da617b1e091e57276b1ccc6
schoobsie <calebovitt09@gmail.com>
2025-10-09 17:01:51 -0400

    0.1

---
+++ HollowKnight.js
@@ -0,0 +1,50 @@
+
+var duration = 60 * 1000;
+var animationEnd = Date.now() + duration;
+var skew = 1;
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+  var timeLeft = animationEnd - Date.now();
+  var ticks = Math.max(200, 500 * (timeLeft / duration));
+  skew = Math.max(0.8, skew - 0.001);
+
+  confetti({
+    particleCount: Math.round(Math.random()),
+    startVelocity: 0,
+    ticks: ticks,
+    origin: {
+      x: Math.random(),
+      y: (Math.random() * skew) - 0.2
+    },
+    colors: ['#858585'],
+    shapes: ['circle'],
+    gravity: randomInRange(0.1, 0.2),
+    scalar: randomInRange(0.4, 1),
+    drift: randomInRange(0.1, 0.6)
+  });
+
+  if (timeLeft > 0) {
+    setTimeout(() => requestAnimationFrame(frame), 50);
+  }
+}());
+
+function glowFunction(event) {
+    const textElement = event.currentTarget.querySelector('p');
+
+    textElement.classList.add('active');
+
+
+    if (textElement) {
+        textElement.style.transition = 'text-shadow 0.7s ease-in-out';
+        textElement.style.textShadow = '0 0 20px rgba(255, 255, 255, 1), 0 0 30px rgba(255, 255, 255, 0.9)';
+
+    setTimeout(() => {
+        textElement.style.textShadow = 'none';
+        textElement.classList.remove('active');
+    }, 500);
+  }
+}
